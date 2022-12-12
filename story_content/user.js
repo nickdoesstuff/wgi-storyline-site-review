b950cad8e2a45f51f74a752e7aee083628ad7709
Nicholas A Shelton <nickshelton1376@gmail.com>
2022-12-11 20:13:12 -0500

    Initial Commit

---
+++ story_content/user.js
@@ -0,0 +1,103 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6X4jGRWv8yp":
+        Script1();
+        break;
+      case "6RlWZweGFy9":
+        Script2();
+        break;
+      case "6Wrg7XtlAlW":
+        Script3();
+        break;
+      case "6TlKfTdkrrQ":
+        Script4();
+        break;
+      case "5cahIvmOQsS":
+        Script5();
+        break;
+      case "6bNtalJVc5V":
+        Script6();
+        break;
+      case "5j5IsyMAoE6":
+        Script7();
+        break;
+      case "6j5Ttzqwu02":
+        Script8();
+        break;
+      case "6W6MHrwuMGR":
+        Script9();
+        break;
+      case "6ANu0Em5iYe":
+        Script10();
+        break;
+      case "6HtN0luwVik":
+        Script11();
+        break;
+      case "6bjI2C8SvsV":
+        Script12();
+        break;
+  }
+}
+
+function Script1()
+{
+  document.body.style.backgroundColor = "#305399";
+}
+
+function Script2()
+{
+  alert("This isn't functional, but could link to key content in the final version of the site");
+}
+
+function Script3()
+{
+  alert("This isn't functional, but could eventually store information such as a users notes, bookmarks to content, and potentially a leaderboard.");
+}
+
+function Script4()
+{
+  alert("This isn't functional, but could eventually allow a user to share links to the site and specific content by copying a link and/or sending an email.");
+}
+
+function Script5()
+{
+  document.body.style.backgroundColor = "#DF3416";
+}
+
+function Script6()
+{
+  alert("This isn't functional, but could link to key content in the final version of the site");
+}
+
+function Script7()
+{
+  alert("This isn't functional, but could eventually store information such as a users notes, bookmarks to content, and potentially a leaderboard.");
+}
+
+function Script8()
+{
+  alert("This isn't functional, but could eventually allow a user to share links to the site and specific content by copying a link and/or sending an email.");
+}
+
+function Script9()
+{
+  document.body.style.backgroundColor = "#CDE7F9";
+}
+
+function Script10()
+{
+  alert("This isn't functional, but could link to key content in the final version of the site");
+}
+
+function Script11()
+{
+  alert("This isn't functional, but could eventually store information such as a users notes, bookmarks to content, and potentially a leaderboard.");
+}
+
+function Script12()
+{
+  alert("This isn't functional, but could eventually allow a user to share links to the site and specific content by copying a link and/or sending an email.");
+}
+
